16c33fafbbe2a61bc0b39a48fba293a978fe2d6b
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-22 00:52:58 +0000

    add formatIssues

---
+++ packages/utils/src/_removeSchemaArg/_removeSchemaArg.ts
@@ -0,0 +1,23 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+
+export type SchemaArgs<Args extends unknown[]> =
+  | Args
+  | [schema: StandardSchemaV1, ...args: Args];
+
+/**
+ * Some functions take a schema as a first argument for inference purposes.
+ * This function removes the schema from the arguments if it exists.
+ *
+ * @param args The arguments to remove the schema from.
+ *
+ * @returns The arguments without the schema.
+ */
+export function _removeSchemaArg<Args extends unknown[]>(
+  args: SchemaArgs<Args>,
+): Args {
+  const schema = args[0];
+  if (schema && (schema as StandardSchemaV1)["~standard"]) {
+    return args.slice(1) as Args;
+  }
+  return args as Args;
+}
