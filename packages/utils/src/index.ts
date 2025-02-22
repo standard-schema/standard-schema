3397eb540b4a9d36ad7dd38ada67db1b561a8360
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-22 00:58:29 +0000

    make sure flattenIssues and formatIssues are exported

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./getDotPath/index.ts";
 export * from "./SchemaError/index.ts";
 export * from "./getPathSegmentKey/index.ts";
+export * from "./flattenIssues/index.ts";
+export * from "./formatIssues/index.ts";
