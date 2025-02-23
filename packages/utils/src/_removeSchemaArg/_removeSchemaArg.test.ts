78146e12ec0c6430c848e65025d7acb769d7939a
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-23 17:51:42 +0000

    add isStandardSchema guard

--- packages/utils/src/_removeSchemaArg/_removeSchemaArg.test.ts
+++ packages/utils/src/_removeSchemaArg/_removeSchemaArg.test.ts
@@ -1,18 +1,11 @@
-import type { StandardSchemaV1 } from "@standard-schema/spec";
 import { describe, expect, test } from "vitest";
+import { stringSchema } from "../__test_fixtures/index.ts";
 import { _removeSchemaArg } from "./_removeSchemaArg.ts";
 
 describe("_removeSchemaArg", () => {
-  const schema: StandardSchemaV1 = {
-    "~standard": {
-      vendor: "custom",
-      version: 1,
-      validate: () => ({ issues: [] }),
-    },
-  };
   const withoutSchema = ["foo", "bar"];
   test("should return the arguments without the schema", () => {
-    expect(_removeSchemaArg([schema, ...withoutSchema])).toStrictEqual(
+    expect(_removeSchemaArg([stringSchema, ...withoutSchema])).toStrictEqual(
       withoutSchema,
     );
   });
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,3 +3,4 @@ export * from "./SchemaError/index.ts";
 export * from "./getPathSegmentKey/index.ts";
 export * from "./flattenIssues/index.ts";
 export * from "./formatIssues/index.ts";
+export * from "./isStandardSchema/index.ts";
