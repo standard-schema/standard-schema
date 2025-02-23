060f29ef04e45a7c9ef6797292e542fa9eeac191
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-23 17:51:42 +0000

    add isStandardSchema guard

---
+++ packages/utils/src/__test_fixtures/index.ts
@@ -0,0 +1,31 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+
+export const stringSchema: StandardSchemaV1<string> = {
+  "~standard": {
+    version: 1,
+    vendor: "custom",
+    validate: (value) => {
+      const pass = typeof value === "string";
+      return pass
+        ? { value }
+        : {
+            issues: [
+              { message: `Expected string, got ${typeof value}`, path: [] },
+            ],
+          };
+    },
+  },
+};
+
+export interface Fields {
+  foo: string;
+  bar: number;
+}
+
+export const fieldsSchema: StandardSchemaV1<Fields> = {
+  "~standard": {
+    version: 1,
+    vendor: "custom",
+    validate: () => ({ issues: [{ message: "Not implemented" }] }),
+  },
+};
