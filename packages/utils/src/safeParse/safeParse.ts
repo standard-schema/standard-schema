e2dfd73d5944c3c7fe696cb039e1da96375e1073
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-26 11:26:46 +0000

    add basic parsing utils

---
+++ packages/utils/src/safeParse/safeParse.ts
@@ -0,0 +1,41 @@
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+import type { LooseAutocomplete } from "../types.ts";
+
+/**
+ * Parse unknown data with a schema, returning a result object.
+ *
+ * @param schema The schema to parse the data with.
+ *
+ * @param data The data to parse.
+ *
+ * @returns A promise that resolves to a result object.
+ */
+export async function safeParse<Schema extends StandardSchemaV1>(
+  schema: Schema,
+  data: LooseAutocomplete<StandardSchemaV1.InferInput<Schema>>,
+): Promise<StandardSchemaV1.Result<StandardSchemaV1.InferOutput<Schema>>> {
+  return schema["~standard"].validate(data);
+}
+
+/**
+ * Parse unknown data with a schema synchronously, returning a result object.
+ * Throws an error if the schema validation is asynchronous.
+ *
+ * @param schema The schema to parse the data with.
+ *
+ * @param data The data to parse.
+ *
+ * @returns A result object.
+ *
+ * @throws {TypeError} If the schema validation is asynchronous.
+ */
+export function safeParseSync<Schema extends StandardSchemaV1>(
+  schema: Schema,
+  data: LooseAutocomplete<StandardSchemaV1.InferInput<Schema>>,
+): StandardSchemaV1.Result<StandardSchemaV1.InferOutput<Schema>> {
+  const result = schema["~standard"].validate(data);
+  if (result instanceof Promise) {
+    throw new TypeError("Schema validation must be synchronous");
+  }
+  return result;
+}
