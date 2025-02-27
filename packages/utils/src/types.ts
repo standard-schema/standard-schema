dd53684a88272ad3e1fdc488ef235221c1524072
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-27 11:42:48 +0000

    move keyofunion type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,3 +1,4 @@
 export type NotNullish = NonNullable<unknown>;
 export type NonReducibleUnknown = NotNullish | null | undefined;
 export type LooseAutocomplete<T> = T | NonReducibleUnknown;
+export type KeyofUnion<T> = T extends unknown ? keyof T : never;
