d19d7b3a994a788897a4ddf194002010b9b4b1a6
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-20 11:59:34 +0100

    port summarize from valibot and use for SchemaError
    original valibot PR: https://github.com/fabian-hiller/valibot/pull/1158

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,4 +6,5 @@ export * from "./formatIssues/index.ts";
 export * from "./isStandardSchema/index.ts";
 export * from "./parse/index.ts";
 export * from "./safeParse/index.ts";
+export * from "./summarize/index.ts";
 export * from "./types/index.ts";
