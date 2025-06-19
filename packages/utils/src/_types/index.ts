21d09b650f15ac6cf38ae31d85028ce7550da4c2
Ben Durrant <ben.durrant@marketdojo.com>
2025-06-19 19:49:07 +0100

    add (safe)parseDictionary/Tuple utils

--- packages/utils/src/_types/index.ts
+++ packages/utils/src/_types/index.ts
@@ -1,4 +1,4 @@
 export type NotNullish = NonNullable<unknown>;
 export type NonReducibleUnknown = NotNullish | null | undefined;
-export type LooseAutocomplete<T> = T | NonReducibleUnknown;
+export type LooseAutocomplete<T, BaseType = NonReducibleUnknown> = T | BaseType;
 export type KeyofUnion<T> = T extends unknown ? keyof T : never;
