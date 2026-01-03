a18023f9a4e9ab164a5a4cea30664b8a78e08b39
Ben Durrant <ben.durrant@marketdojo.com>
2026-01-03 11:29:44 +0000

    add null checks for standard props

--- packages/utils/src/isStandardJsonSchema/isStandardJsonSchema.ts
+++ packages/utils/src/isStandardJsonSchema/isStandardJsonSchema.ts
@@ -2,28 +2,29 @@ import type { StandardJSONSchemaV1 } from "@standard-schema/spec";
 
 // check for any version
 export function isStandardJsonSchema(
-  schema: unknown,
+	schema: unknown,
 ): schema is StandardJSONSchemaV1;
 // check for specific version
 export function isStandardJsonSchema(
-  schema: unknown,
-  version: 1,
+	schema: unknown,
+	version: 1,
 ): schema is StandardJSONSchemaV1;
 export function isStandardJsonSchema(
-  // biome-ignore lint/suspicious/noExplicitAny: we don't want to typeof check the schema itself - only the ~standard property (schemas can be objects or functions, for example)
-  schema: any,
-  version?: number,
+	// biome-ignore lint/suspicious/noExplicitAny: we don't want to typeof check the schema itself - only the ~standard property (schemas can be objects or functions, for example)
+	schema: any,
+	version?: number,
 ): schema is StandardJSONSchemaV1 {
-  if (schema == null) return false;
-  const standardProps = schema["~standard"];
-  const baseConditions =
-    typeof standardProps === "object" &&
-    typeof standardProps.jsonSchema === "object" &&
-    typeof standardProps.jsonSchema.input === "function" &&
-    typeof standardProps.jsonSchema.output === "function" &&
-    typeof standardProps.version === "number" &&
-    typeof standardProps.vendor === "string";
-  return version == null
-    ? baseConditions
-    : baseConditions && standardProps.version === version;
+	if (schema == null) return false;
+	const standardProps = schema["~standard"];
+	const baseConditions =
+		typeof standardProps === "object" &&
+		standardProps != null &&
+		typeof standardProps.jsonSchema === "object" &&
+		typeof standardProps.jsonSchema.input === "function" &&
+		typeof standardProps.jsonSchema.output === "function" &&
+		typeof standardProps.version === "number" &&
+		typeof standardProps.vendor === "string";
+	return version == null
+		? baseConditions
+		: baseConditions && standardProps.version === version;
 }
--- packages/utils/src/isStandardSchema/isStandardSchema.ts
+++ packages/utils/src/isStandardSchema/isStandardSchema.ts
@@ -4,22 +4,23 @@ import type { StandardSchemaV1 } from "@standard-schema/spec";
 export function isStandardSchema(schema: unknown): schema is StandardSchemaV1;
 // check for specific version
 export function isStandardSchema(
-  schema: unknown,
-  version: 1,
+	schema: unknown,
+	version: 1,
 ): schema is StandardSchemaV1;
 export function isStandardSchema(
-  // biome-ignore lint/suspicious/noExplicitAny: we don't want to typeof check the schema itself - only the ~standard property (schemas can be objects or functions, for example)
-  schema: any,
-  version?: number,
+	// biome-ignore lint/suspicious/noExplicitAny: we don't want to typeof check the schema itself - only the ~standard property (schemas can be objects or functions, for example)
+	schema: any,
+	version?: number,
 ): schema is StandardSchemaV1 {
-  if (schema == null) return false;
-  const standardProps = schema["~standard"];
-  const baseConditions =
-    typeof standardProps === "object" &&
-    typeof standardProps.validate === "function" &&
-    typeof standardProps.version === "number" &&
-    typeof standardProps.vendor === "string";
-  return version == null
-    ? baseConditions
-    : baseConditions && standardProps.version === version;
+	if (schema == null) return false;
+	const standardProps = schema["~standard"];
+	const baseConditions =
+		typeof standardProps === "object" &&
+		standardProps != null &&
+		typeof standardProps.validate === "function" &&
+		typeof standardProps.version === "number" &&
+		typeof standardProps.vendor === "string";
+	return version == null
+		? baseConditions
+		: baseConditions && standardProps.version === version;
 }
